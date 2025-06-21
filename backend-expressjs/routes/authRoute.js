a99973e55d90f8a951b1ada2ad5c34189ad2e988
Rajib Salui <rajibsalui2003@gmail.com>
2025-06-21 10:54:44 +0530

    fix: remove authMiddleware from registration and login routes

--- backend-expressjs/routes/authRoute.js
+++ backend-expressjs/routes/authRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 import { register, login } from '../controllers/authController.js';
 import authMiddleware from '../middlewares/authMiddleware.js';
 
-router.post('/register', authMiddleware, register);
-router.post('/login', authMiddleware, login);
+router.post('/register', register);
+router.post('/login',  login);
 
 export default router; 
