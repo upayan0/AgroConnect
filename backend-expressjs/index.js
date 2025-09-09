4e29c2a7800f33129d9071ebd33f77af92056cd2
upayan0 <upayanchatterjee7@gmail.com>
2025-09-09 23:24:09 +0530

    dhemna rajib push

--- backend-expressjs/index.js
+++ backend-expressjs/index.js
@@ -13,14 +13,30 @@ const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/agroconnec
 
 app.use(bodyParser.json());
 
-// cors middleware
-app.use(cors({
-    origin: ['http://localhost:8080','http://localhost:8081','https://agro-connect-y6nl-git-main-upayanchatterjee7-gmailcoms-projects.vercel.app','https://agro-connect-y6nl-upayanchatterjee7-gmailcoms-projects.vercel.app','https://agro-connect-y6nl.vercel.app','https://agro-connect-p7j2.vercel.app'],
-    //origin:true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-}));
+// // cors middleware
+// app.use(cors({
+//     origin: ['http://localhost:8080','http://localhost:8081','https://agro-connect-y6nl-git-main-upayanchatterjee7-gmailcoms-projects.vercel.app','https://agro-connect-y6nl-upayanchatterjee7-gmailcoms-projects.vercel.app','https://agro-connect-y6nl.vercel.app','https://agro-connect-p7j2.vercel.app'],
+//     //origin:true,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+//     credentials: true,
+// }));
+
+
+// CORS middleware at the very top
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://agro-connect-y6nl-git-main-upayanchatterjee7-gmailcoms-projects.vercel.app");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+
 // Handle preflight requests
 app.options('*', cors());
 
